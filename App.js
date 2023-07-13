dc399a56a2cd1bcd78d0da94a0ee0b9262f3a791
yusuf <kingshakzy@gmail.com>
2023-07-13 19:19:20 +0100

    Done with first landing page, although code isn't optimised

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, StatusBar, ImageBackground, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, StatusBar, ImageBackground, Image, Button, Pressable } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp} from "react-native-responsive-screen";
 import AppLoading from 'expo-app-loading';
 import { useFonts,  Manrope_200ExtraLight,
@@ -30,18 +30,37 @@ export default function App() {
         {/* <StatusBar barStyle="light-content" backgroundColor="transparent" /> */}
         <ImageBackground source={require('./assets/images/haircut.jpg')} 
         style={{...styles.container}} 
-        resizeMode="cover" imageStyle={{opacity: 0.4}} className={"bg-gray-800 pt-60 flex-1 items-center"}>
+        resizeMode="cover" imageStyle={{opacity: 0.4}} className={"bg-gray-800 pt-40 flex-1 items-center"}>
           <Image
             source={require('./assets/adaptive-icon.png')}
             className={'w-32'}
           />
           <View className={"w-auto flex items-center text-center"}>
-            <Text className={"text-3xl text-white"} style={{fontFamily: "Manrope_200ExtraLight"}}>
+            <Text className={"text-4xl text-white mt-8"} style={{fontFamily: "Manrope_700Bold"}}>
               Let's beautify you!
             </Text>
-            <Text className={"w-auto text-center text-lg text-white font-['Manrope_200ExtraLight']"} style={{fontFamily: "Manrope_200ExtraLight"}}>
+            <Text className={"text-center text-lg text-white "} style={{fontFamily: "Manrope_400Regular", width: width*0.91}}>
               We specialize in haircut, hairlock, tattoo, manicure, pedicure and ear piercing
             </Text>
+          </View>
+          <View className={"flex-1 flex-row justify-center mt-16"} style={{width: width*0.5}}>
+              <View className={"rounded-full bg-white mr-2"} style={{width: 20, height: 20,}}>
+
+              </View>
+              <View className={"rounded-full border-[1px] border-white mr-2"} style={{width: 20, height: 20,}}>
+
+              </View>
+              <View className={"rounded-full border-[1px] border-white mr-2"} style={{width: 20, height: 20,}}>
+
+              </View>
+          </View>
+          <View style={{flex:1, justifyContent: "space-between", flexDirection: "row", width: width*0.8, paddingInline: 10}}>
+            <Pressable style={{flex: 4, height: height*0.07, alignItems: 'center', justifyContent: "center", marginRight: 8}} className={"rounded-xl text-white border-[1px] border-white bg-transparent"}>
+              <Text className={"text-white"}>Skip</Text>
+            </Pressable>
+            <Pressable style={{flex: 4, height: height*0.07, alignItems: 'center', justifyContent: "center", backgroundColor: "green"}} className={"bg-green text-white rounded-xl border-white bg-transparent"}>
+              <Text className={"text-white"}>Next</Text>
+            </Pressable>
           </View>
         </ImageBackground>
       </View>
